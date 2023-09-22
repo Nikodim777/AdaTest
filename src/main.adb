76647ce43321e462ac24a7be8db2412f7d0caf8a
Nikodim777 <gorshenin.lev@gmail.com>
2023-09-22 23:01:04 +0300

    The simple program created.

--- src/main.adb
+++ src/main.adb
@@ -1,6 +1,19 @@
-with Ada.Text_IO;
-procedure Main is
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 
+procedure Main is
+  subtype Speed is Integer range 0 .. 1000;
+  subtype Time is Integer range 0 .. 100;
+  S : Speed;
+  T : Time;
+  D : constant Integer := 285;
 begin
-  Ada.Text_IO.Put_Line ("Hello, world!!");
+  Put ("Введите скорость в км/ч: ");
+  Get (S);
+  Put ("Введите время в часах: ");
+  Get (T);
+  Put ("Проехав заданное время с заданной скоростью вы остановитесь на ");
+  Put (S * T rem D, 3);
+  Put (" километре");
+  New_Line;
 end Main;
